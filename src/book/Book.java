235895afb73951b1583c8eb1adc6aade71a2b775
musi9000 <mustafa@Mustafas-MacBook-Pro.local>
2016-05-26 00:53:08 +0100

    update

--- src/book/Book.java
+++ src/book/Book.java
@@ -58,28 +58,5 @@ public class Book {
     public void printBookInfo(){
         System.out.printf("Заглавие: %s автор: %s издателство: %s ISBN: %s\n",title,author,publication,isbn);
     }
-    public static void main(String[] args) {
-        Library lib = new Library("BookStore");
-
-        lib.addBook("Аз и майор Блюхер", "Васил Пармаков", "Black Flamingo", "9789542915027");
-        lib.addBook("Анатол", "Артур Шницлер", "Black Flamingo", "9789542915089");
-        lib.addBook("Тук и сега", "Роланд Шимелпфениг", "Black Flamingo", "9789542915034");
-        lib.addBook("22 ноември 1963", "Стивън Кинг", "Плеяда", "9789544093259");
-        lib.addBook("Възкресяване", "Стивън Кинг", "Плеяда", "9789544093518");
-
-        /*lib.allBooksInfo();
-        System.out.print("----------------------\n");
-
-        System.out.print("Търсене на книга зададен автор (Стивън Кинг) - ");
-        lib.searchBookByAuthor("Стивън Кинг");
-        
-        System.out.print("Изтриване на книга зададен автор (Толстой) - ");
-        lib.deleteBookByAuthor("Толстой");
-        */
-        System.out.print("Изтриване на книга зададен автор (Стивън Кинг) - ");
-        lib.deleteBookByAuthor("Стивън Кинг");
-        
-        System.out.print("----------------------\n");
-        lib.allBooksInfo();
-    }
+    
 }
